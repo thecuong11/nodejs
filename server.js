5779a49ffb6ed8a1f2e3fefec5227ffc415cfe38
thecuong11 <thecuong98lnbg@gmail.com>
2023-11-22 15:15:49 +0700

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 3052
+const PORT = process.env.PORT || 3051
 
 const server = app.listen( PORT, () =>{
     console.log(`WSV eCommerce start with ${PORT}`)
